665401b07709b68b4b956925832d66d25e38fa34
Erin Shepherd <erin.shepherd@e43.eu>
2023-12-22 00:42:50 +0100

    fix: Reskin  * Update logo  * Menu -> Point LG link at Alice  * Change BG colour

---
+++ resources/skins/fix/layouts/ixp-logo-header.foil.php
@@ -0,0 +1,4 @@
+<a class="navbar-brand" style="font-size:18px" href="<?= url('') ?>">
+    <img class="tw-inline" style="max-height: 26px !important; margin-right: 1rem !important;" src="https://fix-berlin.net/FIX-Berlin-W561.png">
+    <?= config('identity.titlename' ) ?>
+</a>
