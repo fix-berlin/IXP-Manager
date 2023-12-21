6d8d3c052207a4824f40d5ad0656e3d190e5ce13
Erin Shepherd <erin.shepherd@e43.eu>
2023-12-22 00:47:03 +0100

    fix: Adjust IXP logo header slightly

--- resources/skins/fix/layouts/ixp-logo-header.foil.php
+++ resources/skins/fix/layouts/ixp-logo-header.foil.php
@@ -1,4 +1,4 @@
 <a class="navbar-brand" style="font-size:18px" href="<?= url('') ?>">
-    <img class="tw-inline" style="max-height: 26px !important; margin-right: 1rem !important;" src="https://fix-berlin.net/FIX-Berlin-W561.png">
-    <?= config('identity.titlename' ) ?>
+    <img class="tw-inline" style="max-height: 26px !important; margin-right: 1rem !important;" src="https://fix-berlin.net/FIX-W200.png">
+    IXP Manager
 </a>
