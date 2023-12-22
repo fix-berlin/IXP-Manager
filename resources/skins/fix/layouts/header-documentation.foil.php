acf866f231a2fa0e159364e1dd38e7dd5b286e32
Erin Shepherd <erin.shepherd@e43.eu>
2023-12-22 22:12:42 +0100

    Add custom documentation header

---
+++ resources/skins/fix/layouts/header-documentation.foil.php
@@ -0,0 +1,15 @@
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle center-dd-caret d-flex" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Documentation
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="https://docs.fix-berlin.net/connecting">Getting Connected</a>
+        <a class="dropdown-item" href="https://docs.fix-berlin.net/rules">Rules</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="https://docs.fix-berlin.net/route_servers">Route Servers</a>
+        <div class="dropdown-divider"></div>
+        <h6 class="dropdown-header">Official IXP Manager Sites</h6>
+        <a class="dropdown-item" href="https://www.ixpmanager.org/" target="_blank">Homepage</a>
+        <a class="dropdown-item" href="https://docs.ixpmanager.org/" target="_blank">Documentation</a>
+    </div>
+</li>
