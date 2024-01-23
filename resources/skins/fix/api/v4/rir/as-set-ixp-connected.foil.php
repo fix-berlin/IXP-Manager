9f0f9e20402370289c73ecb31dba73e481553cc7
Erin Shepherd <erin.shepherd@e43.eu>
2024-01-23 13:25:51 +0100

    Add templates to generate RIR obejcts

---
+++ resources/skins/fix/api/v4/rir/as-set-ixp-connected.foil.php
@@ -0,0 +1,15 @@
+password: <?= config('ixp_api.rir.password') ?>
+
+
+    as-set:         AS198136:AS-PEERS
+    descr:          FIX Berlin Peers
+    admin-c:        SN9448-RIPE
+    tech-c:         SN9448-RIPE
+    remarks:        IXP member ASNs connected to the route servers are listed in AS198136:AS-CONNECTED
+<?php foreach( $t->asns as $asn => $details ): ?>
+    members:        <?= $details[ "asmacro" ] ?>
+
+<?php endforeach; ?>
+    mnt-by:         FIX-AUTO-MNT
+    mnt-by:         SEPC-MNT
+    source:         RIPE
---
+++ resources/skins/fix/api/v4/rir/autnum.foil.php
@@ -0,0 +1,19 @@
+password: <?= config('ixp_api.rir.password') ?>
+
+
+    aut-num:        198136
+    as-name:        FIX-RS
+    descr:          FIX Berlin Routeservers
+    org:            ORG-SEPC3-RIPE
+    sponsoring-org: ORG-ACP10-RIPE
+<?php foreach( $t->asns as $asn => $details ): ?>
+    import:         from AS<?= $asn ?> accept <?= $details[ "asmacro" ] ?> # <?= $details[ "name" ] ?>
+
+    export:         to   AS<?= $asn ?> announce AS198136:AS-RS-PEERS
+<?php endforeach; ?>
+    admin-c:        SN9448-RIPE
+    tech-c:         SN9448-RIPE
+    status:         ASSIGNED
+    mnt-by:         FIX-AUTO-MNT
+    mnt-by:         SEPC-MNT
+    source:         RIPE
