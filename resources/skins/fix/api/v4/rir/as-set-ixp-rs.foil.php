ceb4e69c715fdb0ce828f40693508b2169d267cb
Erin Shepherd <erin.shepherd@e43.eu>
2024-01-23 13:49:52 +0100

    RIR objects: Remove indent

--- resources/skins/fix/api/v4/rir/as-set-ixp-rs.foil.php
+++ resources/skins/fix/api/v4/rir/as-set-ixp-rs.foil.php
@@ -1,17 +1,17 @@
 password: <?= config('ixp_api.rir.password') ?>
 
 
-    as-set:         AS198136:AS-RS-PEERS
-    descr:          ASNs connected to the Route Server system at IXP
-    admin-c:        SN9448-RIPE
-    tech-c:         SN9448-RIPE
-    remarks:        IXP member ASNs are listed in AS198136:AS-CONNECTED
+as-set:         AS198136:AS-RS-PEERS
+descr:          ASNs connected to the Route Server system at IXP
+admin-c:        SN9448-RIPE
+tech-c:         SN9448-RIPE
+remarks:        IXP member ASNs are listed in AS198136:AS-CONNECTED
 <?php foreach( $t->customers as $c ): ?>
 <?php if( $c->routeServerClient( ) ): ?>
-    members:        <?= $c->asMacro( 4, 'AS' ) ?>
+members:        <?= $c->asMacro( 4, 'AS' ) ?>
 
 <?php endif; ?>
 <?php endforeach; ?>
-    mnt-by:         FIX-AUTO-MNT
-    mnt-by:         SEPC-MNT
-    source:         RIPE
+mnt-by:         FIX-AUTO-MNT
+mnt-by:         SEPC-MNT
+source:         RIPE
